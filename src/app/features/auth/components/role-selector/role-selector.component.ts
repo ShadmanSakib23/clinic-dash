4468325abc5f7828be973c8b4023318f6d2e0d14
ShadmanSakib23 <Shadman.Sakib@brainstation-23.com>
2025-12-24 14:58:25 +0600

    add project structure with defined module boundaries

---
+++ src/app/features/auth/components/role-selector/role-selector.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-role-selector',
+  imports: [],
+  templateUrl: './role-selector.component.html',
+  styleUrl: './role-selector.component.scss',
+})
+export class RoleSelectorComponent {
+
+}
